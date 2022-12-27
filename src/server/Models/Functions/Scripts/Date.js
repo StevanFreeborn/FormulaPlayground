ae74152ff430a5f34ce5627d348419acacf2f749
Stevan Freeborn <sfreeborn7@gmail.com>
2022-12-27 15:31:01 -0600

    fix: added brackets

--- src/server/Models/Functions/Scripts/Date.js
+++ src/server/Models/Functions/Scripts/Date.js
@@ -1,15 +1,28 @@
 Date = (function (JSDate) {
   var newDate = function () {
-    if (!(this instanceof newDate) && arguments.length == 3)
+    if (!(this instanceof newDate) && arguments.length == 3) {
       return _Date(arguments[0], arguments[1], arguments[2]);
-    if (arguments.length == 0) return new JSDate();
-    if (arguments.length == 1) return new JSDate(arguments[0]);
-    if (arguments.length == 2) return new JSDate(arguments[0], arguments[1]);
-    if (arguments.length == 3)
+    }
+
+    if (arguments.length == 0) {
+      return new JSDate();
+    }
+
+    if (arguments.length == 1) {
+      return new JSDate(arguments[0]);
+    }
+
+    if (arguments.length == 2) {
+      return new JSDate(arguments[0], arguments[1]);
+    }
+
+    if (arguments.length == 3) {
       return new JSDate(arguments[0], arguments[1], arguments[2]);
-    if (arguments.length == 4)
+    }
+    if (arguments.length == 4) {
       return new JSDate(arguments[0], arguments[1], arguments[2], arguments[3]);
-    if (arguments.length == 5)
+    }
+    if (arguments.length == 5) {
       return new JSDate(
         arguments[0],
         arguments[1],
@@ -17,7 +30,9 @@ Date = (function (JSDate) {
         arguments[3],
         arguments[4]
       );
-    if (arguments.length == 6)
+    }
+
+    if (arguments.length == 6) {
       return new JSDate(
         arguments[0],
         arguments[1],
@@ -26,7 +41,9 @@ Date = (function (JSDate) {
         arguments[4],
         arguments[5]
       );
-    if (arguments.length == 7)
+    }
+
+    if (arguments.length == 7) {
       return new JSDate(
         arguments[0],
         arguments[1],
@@ -36,6 +53,7 @@ Date = (function (JSDate) {
         arguments[5],
         arguments[6]
       );
+    }
   };
 
   newDate.UTC = JSDate.UTC;
