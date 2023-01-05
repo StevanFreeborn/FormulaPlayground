e2508102d2419f0bee06c8ac07a5eafcf40b2c2e
Stevan Freeborn <sfreeborn7@gmail.com>
2023-01-04 20:58:02 -0600

    fix: remove todo about adding context to function base

--- src/server/Models/Functions/FunctionBase.cs
+++ src/server/Models/Functions/FunctionBase.cs
@@ -2,10 +2,6 @@ namespace server.Models.Functions;
 
 public abstract class FunctionBase
 {
-  // TODO: I think I want to provide the formula context
-  // to this class so I can reference it within functions
-  // could be used for interpreting raw date/time info
-  // or validating fields or record values
   protected abstract string Name {get; }
   
   protected abstract object Function(params object[] arguments);
